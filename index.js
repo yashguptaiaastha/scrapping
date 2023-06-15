c6a9808d86dd2233902817b984af4490f03070ff
yashguptaiaastha <yash.gupta@iaastha.com>
2023-06-15 10:23:02 +0530

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const puppeteer = require('puppeteer');
 
-async function run()  {
-    const browser = await puppeteer.launch({headless: false});
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: false,
+        userDataDir: "./tmp"
+    });
     const page = await browser.newPage();
-    await page.goto('https://www.amazon.in/All-Mobile-Phones/s?k=All+Mobile+Phones');
+    await page.goto("https://www.amazon.in/All-Mobile-Phones/s?k=All+Mobile+Phones");
     
-    const html = await page.content();
-    console.log(html);
-   
-    const title=await page.evaluate(() => document.title);
-    console.log(title);
-    const text = await page.evaluate(() => document.body.innerText);
-    console.log(text);
-    //const rating = await page.evaluate(() => rating);
-    //console.log(rating);
+    let result = [];
+    productsHandles = await page.$$(".s-main-slot.s-result-list.s-search-results.sg-row > div > div ");
+    console.log(productsHandles);
+    for (let product of productsHandles){
+        try {
+
+            const title = await page.evaluate(
+            (el) => el.querySelector(" h2 > a > span").textContent, product);
+
+            console.log(title)
     
+        
+            let price = await page.evaluate(
+            (el) => el.querySelector(".a-price-whole").textContent, product);
 
-   await browser.close();
-} 
+            console.log(price);
 
-run();
+            let rating = await page.evaluate(
+                (el) => el.querySelector(".a-icon.a-icon-star-small.a-star-small-4-5.aok-align-bottom").textContent, product);
+    
+                console.log(rating);
+                console.log(result.append(title,price,rating));
+        
+        }
+        catch (error){}
+        //console.log(title)
+       
+
+    }
+    await browser.close();
+})
+();
