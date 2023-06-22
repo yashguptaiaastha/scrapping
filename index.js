935c207e8edf3ef1e8f2a387d98f0e7aff6e410d
yashguptaiaastha <yash.gupta@iaastha.com>
2023-06-22 15:13:00 +0530

    first

--- index.js
+++ index.js
@@ -1,43 +1,75 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: false,
-        userDataDir: "./tmp"
-    });
+//const fs = require("fs");
+const puppeteer = require("puppeteer");
+async function extractdata()
+{
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto("https://www.amazon.in/All-Mobile-Phones/s?k=All+Mobile+Phones");
-    
-    let result = [];
-    productsHandles = await page.$$(".s-main-slot.s-result-list.s-search-results.sg-row > div > div ");
-    console.log(productsHandles);
-    for (let product of productsHandles){
-        try {
+ 
+    // await page.screenshot({path: "image.png",fullPage : true});
+    // await page.pdf({path: "site.pdf",format: "A4"}); 
 
-            const title = await page.evaluate(
-            (el) => el.querySelector(" h2 > a > span").textContent, product);
+    const productsHandles = await page.$$(".sg-col-inner") ;
+    const products = [];
+    let idx = 0;
+    let isBtnDisabled = true;
 
-            console.log(title)
-    
+    while (isBtnDisabled) {
+    const productsHandles = await page.$$(".s-main-slot.s-result-list.s-search-results.sg-row");
+    idx += 1;
+    console.log(idx);
+    for(const productshandle of productsHandles)
+    {
+        let title = "Null";
+        let price = "Null";
+        let rating = "Null";
+        let numberofRating = "Null";
         
-            let price = await page.evaluate(
-            (el) => el.querySelector(".a-price-whole").textContent, product);
+        try{
+            title = await page.evaluate((el) => el.querySelector(".s-title-instructions-style > h2").textContent, productshandle);
+           // console.log("Title :",title);
+        }catch(error){}
 
-            console.log(price);
+        try{
+             price = await page.evaluate((el) => el.querySelector(".a-price-whole").textContent, productshandle);
+          //console.log("Price :",price);
+        }catch(error){}
 
-            let rating = await page.evaluate(
-                (el) => el.querySelector(".a-icon.a-icon-star-small.a-star-small-4-5.aok-align-bottom").textContent, product);
-    
-                console.log(rating);
-                console.log(result.append(title,price,rating));
-        
-        }
-        catch (error){}
-        //console.log(title)
+        try{
+             rating = await page.evaluate((el) => el.querySelector(".a-icon.a-icon-star-small.a-star-small-4.aok-align-bottom").textContent, productshandle);
+            //    console.log("Rating :",rating);
+        }catch(error){}
+
+        try{
+             numberofRating = await page.evaluate((el) => el.querySelector(".a-section.a-spacing-none.a-spacing-top-micro > div > span:nth-child(2) > a > span").textContent, productshandle);
+                //    console.log("NumberofRating :",numberofRating);
        
+        }catch(error){}
+        if (title!== "Null"){
+            products.push({title,price,rating,numberofRating});
+         
 
+        }
+        //    Items.push({title,price,rating,numberofRating,discounts});
+        
     }
-    await browser.close();
-})
-();
+    console.log(products)
+    
+    await page.waitForSelector(".s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator");
+    const nextPageButton = await page.$(".s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator");
+    if (nextPageButton) {
+      await nextPageButton.click();
+      await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    } else {
+      isBtnDisabled = false;
+    }
+    // fs.writeFile("products.json",JSON.stringify(products), (err) => {
+    //     if(err) throw err;
+    //     console.log("File Saved");
+    // })
+  
+    //await browser.close();
+}
+console.log(products);
+}
+extractdata();
